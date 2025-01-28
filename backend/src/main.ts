e2e972fe1fa52d8668ba6af0a89bda8ef0055cdf
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2025-01-28 22:43:59 +0900

    cors0test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:3000';
+  
+
+  app.enableCors({
+    origin: '*', // すべてのオリジンを許可（本番では特定のドメインに制限するのが望ましい）
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   // await app.listen(process.env.PORT ?? 3000);
   const port = process.env.PORT || 3000;
   await app.listen(port);
